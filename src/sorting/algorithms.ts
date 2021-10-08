8e5a69a09636a6a4722e38913260ae3befb717c4
Robin J <robin__j@outlook.com>
2021-10-08 16:07:42 +0530

    added merge, bubble sorts

--- src/sorting/algorithms.ts
+++ src/sorting/algorithms.ts
@@ -1,40 +1,81 @@
-async function partition(arr: any[],minIndex:number,maxIndex:number,comparator:Function,setarray:Function):Promise<number>{
+const delay=1;
+async function partition(arr: any[],minIndex:number,maxIndex:number,setchange:Function):Promise<number>{
     var pivot=maxIndex;
     let i=minIndex-1,j;
     for(j=i+1;j<pivot;j++){
-        if(comparator(arr[j])<comparator(arr[pivot])){
+        if(arr[j].value<arr[pivot].value){
             i+=1;
-            await swap(arr,i,j,setarray);
+            await swap(arr,i,j,setchange);
         }
     }
     i+=1;
-    await swap(arr,i,pivot,setarray);
+    await swap(arr,i,pivot,setchange);
     return i;
 }
 export async function sleep(delay : number){
     await new Promise(r => setTimeout(r, delay));
 }
-async function swap(arr: any[],i:number,j:number,setarray:Function):Promise<void>{
+async function swap(arr: any[],i:number,j:number,setchange:Function):Promise<void>{
     let temp=arr[i];
     arr[i]=arr[j];
     arr[j]=temp;
-    // arr[i].color="blue";
-    // arr[j].color="blue";
-    let t=arr.concat();
-    setarray(t);
-    await sleep(1);
-    // arr[i].color="green";
-    // arr[j].color="green";
-    t=arr.concat();
-    setarray(t);
-    await sleep(1);
-    console.log("not conas;dfoi");
+    setchange(Math.random());
+    await sleep(delay);
 }
-export async function quickSort(arr:any[],minIndex:number,maxIndex:number,comparator:Function,setarray:Function):Promise<void>{
+export async function quickSort(arr:any[],minIndex:number,maxIndex:number,setchange:Function):Promise<void>{
     if(maxIndex<=minIndex){
         return;
     }
-    let pivot=await partition(arr,minIndex,maxIndex,comparator,setarray);
-    quickSort(arr,minIndex,pivot-1,comparator,setarray);
-    quickSort(arr,pivot+1,maxIndex,comparator,setarray);
+    let pivot=await partition(arr,minIndex,maxIndex,setchange);
+    await quickSort(arr,minIndex,pivot-1,setchange);
+    await quickSort(arr,pivot+1,maxIndex,setchange);
+}
+
+export async function mergeSort(arr:any[],low:number,high:number,setchange:Function) {
+    let m= Math.floor((low+high)/2);
+    if(high<=low){
+        return;
+    }
+    await mergeSort(arr,low,m,setchange);
+    await mergeSort(arr,m+1,high,setchange);
+
+    let j=m+1,i=low,k=0;
+    let t=[];
+    for (; i <= m && j<=high;) {
+            if(arr[i].value<arr[j].value){
+                t[k]=arr[i];
+                i++;
+            }else{
+                t[k]=arr[j];
+                j++;
+            }
+            k++;
+    }
+    for(;i<=m;){
+        t[k]=arr[i];
+        i++;
+        k++;
+    }
+    for(;j<=high;){
+        t[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(i=low,k=0;i<=high;i++){
+        arr[i]=t[k++];
+        await sleep(delay);
+        setchange(Math.random());
+    }
+}
+
+export async function bubbleSort(arr:any[],setchange:Function) {
+    let i,j;
+    for(i=0;i<arr.length-1;i++){
+        for(j=i;j<arr.length;j++){
+            if(arr[i].value>arr[j].value){
+                await swap(arr,i,j,setchange);
+            }
+        }
+    }
+    
 }
--- src/sorting/sorting.tsx
+++ src/sorting/sorting.tsx
@@ -1,51 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import {quickSort as alQuickSort, sleep} from './algorithms';
+import React, { FormEventHandler, useEffect, useState } from 'react';
+import { bubbleSort, mergeSort, quickSort, sleep } from './algorithms';
 import './sorting.css';
 export default function Sorting() {
-    interface Bar{
-        value :number,
-        color :string
+    interface Bar {
+        value: number,
+        color: string
     }
+    enum Algorithms{
+        quickSort='Quick Sort',
+        mergeSort='Merge Sort',
+        heapSort='Heap Sort',
+        bubbleSort='Bubble Sort'
+    }
+    const [algo, setalgo] :any= useState(Algorithms.quickSort);
     const [array, setarray] = useState<Bar[]>([]);
-    const size=80;
+    const [change,setchange]:any=useState(0.8);
+    const size = 80;
     useEffect(() => {
         resetArray();
     }, [])
-    function resetArray(){
-        let temp:Bar[]=[];
+    function resetArray() {
+        let temp: Bar[] = [];
         for (let i = 0; i < size; i++) {
-            let min=20,max=500;
-            temp.push({value: Math.floor(Math.random() * (max - min + 1) + min),color:"purple"});
+            let min = 20, max = 500;
+            temp.push({ value: Math.floor(Math.random() * (max - min + 1) + min), color: "purple" });
         }
         setarray(temp);
     }
-    async function quickSort(){
-        let arr=array.concat();
-        alQuickSort(arr,0,arr.length-1,(e:any)=>e.value,setarray);
+    function handleAlgoChange(e:any){
+        setalgo(e.currentTarget.value);
+        resetArray();
     }
+    async function sort() {
+        // let arr = [9,8,7,6,5,4,3,2,1];
+        let arr=array;
+        console.log("before: ",arr);
+        switch (algo) {
+            case Algorithms.quickSort:
+                await quickSort(arr, 0, arr.length - 1, setchange);
+                break;
+            case Algorithms.mergeSort:
+                await mergeSort(arr,0,arr.length-1,setchange);
+                break;
+            case Algorithms.bubbleSort:
+                await bubbleSort(arr,setchange);
+                break;
+            default:
+                break;
+        }
+        console.log("after: ",arr);
+    }
+
     return (
         <>
-        <div className="nav">
-            <select>
-                <option>Sorting</option>
-                <option>Path Finding</option>
-            </select>
-            <button onClick={resetArray}>Reset Array</button>
-            <button onClick={quickSort}>Sort</button>
+            <div className="nav">
+                <h3>{algo}</h3>
+                <label>Type: </label>
+                <select>
+                    <option>Sorting</option>
+                    <option>Path Finding</option>
+                </select>
+                <label>Algorithm: </label>
+                <select onChange={handleAlgoChange}>
+                    <option>{Algorithms.quickSort}</option>
+                    <option>{Algorithms.mergeSort}</option>
+                    <option>{Algorithms.heapSort}</option>
+                    <option>{Algorithms.bubbleSort}</option>
+                </select>
+                <button onClick={resetArray}>Reset Array</button>
+                <button onClick={sort}>Sort</button>
 
-            <div>Quick Sort</div>
-            <button>Quick Sort</button>
-            <button>Merge Sort</button>
-            <button>Heap Sort</button>
-            <button>Bubble Sort</button>
-        </div>
-        <div className="container">
-            <div className="frame">
-                <div className="boundary">{console.log("called again")}
-                {array.map((e:Bar,idx)=><div className="bar" key={idx} style={{height:e.value,background:e.color,width:(window.innerWidth*0.6)/size}}></div>)}
+            </div>
+            <div className="container">
+                <div className="frame">
+                    <div className="boundary">{console.log("rebuilding")}
+                        {array.map((e: Bar, idx) => <div className="bar" key={idx} style={{ height: e.value, background: e.color, width: (window.innerWidth * 0.6) / size }}></div>)}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
